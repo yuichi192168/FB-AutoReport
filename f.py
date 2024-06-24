d8999fed316e76c965d07e21dcb328165e41795a
yuichi192168 <149583086+yuichi192168@users.noreply.github.com>
2024-06-24 22:55:31 +0800

    Update f.py

--- f.py
+++ f.py
@@ -15,13 +15,13 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.get("https://www.facebook.com/")
 
 time.sleep(4)
-driver.find_element_by_id("email").send_keys('9898989898') #replace with your email or phone  number
+driver.find_element_by_id("email").send_keys('kafka2@exdonuts.com') #replace with your email or phone  number
 time.sleep(2)
-driver.find_element_by_id("pass").send_keys('pass123$%') #replace with your password
+driver.find_element_by_id("pass").send_keys('kafka192168') #replace with your password
 time.sleep(2)
 driver.find_element_by_name("login").click()
 time.sleep(4)
-driver.get('https://www.facebook.com/profile.php?id=100089108025261') #replace with account url or that account which you want to report it.
+driver.get('https://www.facebook.com/profile.php?id=100088598930447') #replace with account url or that account which you want to report it.
 time.sleep(4)
 driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[2]/div/div/div/div[1]').click()#three dots
 time.sleep(3)
